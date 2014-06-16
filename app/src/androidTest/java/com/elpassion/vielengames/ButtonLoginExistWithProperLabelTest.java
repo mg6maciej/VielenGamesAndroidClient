c2766efa31fce494f679500f1d181592288b2a9b
Michał Siemiończyk <michal.siemionczyk@elpassion.pl>
2014-06-16 11:51:55 +0200

    added test for login activity button

---
+++ app/src/androidTest/java/com/elpassion/vielengames/ButtonLoginExistWithProperLabelTest.java
@@ -0,0 +1,30 @@
+package com.elpassion.vielengames;
+
+import android.widget.Button;
+
+import com.elpassion.vielengames.ui.LoginActivity;
+
+/**
+ * Created by michalsiemionczyk on 16/06/14.
+ */
+public class ButtonLoginExistWithProperLabelTest extends BaseTestCase<LoginActivity> {
+
+
+    public ButtonLoginExistWithProperLabelTest() {
+        super(LoginActivity.class);
+    }
+
+    public void testButtonExist(){
+        waitForLoginActivity();
+        Button but = (Button) solo.getView(R.id.button_login);
+        assertTrue(but != null);
+    }
+
+    public void testLabelIsProper(){
+        waitForLoginActivity();
+        Button but = (Button) solo.getView(R.id.button_login);
+        assertTrue(but.getText().equals(solo.getString(R.string.button_login_text)));
+    }
+
+
+}
