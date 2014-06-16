d80e46d7abaf7f8d82d6f7b7b55bd988d26aff9b
Maciej Górski <maciek.gorski@gmail.com>
2014-06-16 11:12:50 +0200

    fixed mock build

--- app/src/mock/java/com/elpassion/vielengames/api/MockVielenGamesApiImpl.java
+++ app/src/mock/java/com/elpassion/vielengames/api/MockVielenGamesApiImpl.java
@@ -1,20 +1,16 @@
 package com.elpassion.vielengames.api;
 
 import com.elpassion.vielengames.data.GameProposal;
-import com.elpassion.vielengames.data.Move;
 import com.elpassion.vielengames.data.Player;
 import com.elpassion.vielengames.data.SessionRequest;
 import com.elpassion.vielengames.data.SessionResponse;
 import com.elpassion.vielengames.data.Updates;
-import com.squareup.okhttp.Call;
+import com.elpassion.vielengames.data.kuridor.KuridorMove;
 
 import java.util.List;
 
 import retrofit.Callback;
 import retrofit.client.Response;
-import retrofit.http.Body;
-import retrofit.http.Path;
-import retrofit.http.Query;
 
 public final class MockVielenGamesApiImpl implements VielenGamesApi {
 
@@ -43,6 +39,6 @@ public final class MockVielenGamesApiImpl implements VielenGamesApi {
     }
 
     @Override
-    public void move(Move move, Callback<Response> callback) {
+    public void move(KuridorMove move, Callback<Response> callback) {
     }
 }
