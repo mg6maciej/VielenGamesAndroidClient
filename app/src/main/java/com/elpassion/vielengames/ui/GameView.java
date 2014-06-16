20e1f7c863304f29a8f8ea5c00da376f02ffbc57
Pawel <PawelKobojek@gmail.com>
2014-06-16 15:19:48 +0200

    walls drawing

--- app/src/main/java/com/elpassion/vielengames/ui/GameView.java
+++ app/src/main/java/com/elpassion/vielengames/ui/GameView.java
@@ -5,11 +5,14 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.elpassion.vielengames.data.kuridor.KuridorGameState;
 import com.elpassion.vielengames.data.kuridor.PawnPosition;
 import com.elpassion.vielengames.data.kuridor.PositionConverter;
+import com.elpassion.vielengames.data.kuridor.WallPosition;
 
 /**
  * Created by pawel on 16.06.14.
@@ -18,15 +21,14 @@ public class GameView extends View {
 
     private static final int FIELD_COUNT = 9;
     private static final int BORDER = 5;
+    private static final int WALL_WIDTH = 15;
 
     private Paint paint = new Paint();
-    private KuridorGameState gameState;
 
+    private KuridorGameState gameState;
 
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
-
-        paint.setColor(Color.BLACK);
     }
 
     public void setGameState(KuridorGameState state) {
@@ -43,20 +45,61 @@ public class GameView extends View {
 
         for (int y = 0; y < FIELD_COUNT; ++y) {
             for (int x = 0; x < FIELD_COUNT; ++x) {
+                paint.setColor(Color.BLACK);
                 canvas.drawRect(x * dim, y * dim, (x + 1) * dim, (y + 1) * dim, paint);
 
-                paint.setColor(Color.WHITE);
-                for (PawnPosition pos : gameState.getPawns()) {
-                    int pawnX = PositionConverter.getX(pos.getPosition());
-                    int pawnY = PositionConverter.getY(pos.getPosition());
-
-                    if (pawnX == x && pawnY == y) {
-                        paint.setColor(Color.BLUE);
-                    }
-                }
+                setPlayerColor(x, y);
                 canvas.drawRect(x * dim + BORDER, y * dim + BORDER, (x + 1) * dim - BORDER, (y + 1) * dim - BORDER, paint);
-                paint.setColor(Color.BLACK);
             }
         }
+
+        drawWalls(canvas, dim);
+    }
+
+    private void setPlayerColor(int x, int y) {
+        paint.setColor(Color.WHITE);
+        if (gameState != null) {
+            for (PawnPosition pos : gameState.getPawns()) {
+                int pawnX = PositionConverter.getX(pos.getPosition());
+                int pawnY = PositionConverter.getY(pos.getPosition());
+
+                if (pawnX == x && pawnY == y) {
+                    paint.setColor(Color.BLUE);
+                }
+            }
+        }
+    }
+
+    private void drawWalls(Canvas canvas, int dim) {
+        paint.setColor(Color.RED);
+        if (gameState != null) {
+            for (WallPosition wall : gameState.getWalls()) {
+                int wallX = PositionConverter.getX(wall.getPosition());
+                int wallY = PositionConverter.getY(wall.getPosition());
+
+                PositionConverter.Orientation ornt = PositionConverter.getOrientation(wall.getPosition());
+                float startX = (ornt == PositionConverter.Orientation.hor) ? (wallX - 2) * dim : wallX * dim;
+                float startY = (ornt == PositionConverter.Orientation.hor) ? wallY * dim : (wallY - 2) * dim;
+                float stopX = (ornt == PositionConverter.Orientation.hor) ? wallX * dim : startX + WALL_WIDTH;
+                float stopY = (ornt == PositionConverter.Orientation.hor) ? startY + WALL_WIDTH : wallY * dim;
+
+                Log.d("TAGTAG", String.format("cords = (%d, %d) start = (%f, %f), stop = (%f, %f)", wallX, wallY, startX, startY, stopX, stopY));
+                canvas.drawRect(startX, startY, stopX, stopY, paint);
+
+//                    canvas.drawRect(x * dim, y * dim, (x + 1) * dim, y * dim + WALL_WIDTH, paint);
+            }
+        }
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        // TODO
+        float x = event.getX();
+        float y = event.getY();
+
+
+        return true;
     }
 }
