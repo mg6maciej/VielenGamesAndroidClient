9e1a8941fed863c18b50cdcaa32b4ed2a2d930bd
Maciej Górski <maciek.gorski@gmail.com>
2014-06-19 18:52:28 +0200

    code reformat

--- app/src/main/java/com/elpassion/vielengames/data/kuridor/PositionConverter.java
+++ app/src/main/java/com/elpassion/vielengames/data/kuridor/PositionConverter.java
@@ -1,7 +1,5 @@
 package com.elpassion.vielengames.data.kuridor;
 
-import android.graphics.drawable.GradientDrawable;
-
 /**
  * Created by mateusz on 16/06/2014.
  */
@@ -24,38 +22,38 @@ public class PositionConverter {
         }
     }
 
-        public static int getX(String position) {
-            int i = getOrientation(position) == Orientation.none ? 0 : 1;
+    public static int getX(String position) {
+        int i = getOrientation(position) == Orientation.none ? 0 : 1;
 
-            if (position == null || position.length() <= i)
-                return 0;
+        if (position == null || position.length() <= i)
+            return 0;
 
-            return Math.abs(((int) 'a') - ((int) position.charAt(i)));
-        }
-
-        public static int getY(String position) {
-            int i = getOrientation(position) == Orientation.none ? 1 : 2;
-
-            if (position == null || position.length() <= i)
-                return 0;
-
-            return ((int) '9') - ((int) position.charAt(i));
-        }
-
-        public static Orientation getOrientation(String position) {
-            if (position == null || position.length() < 1)
-                return Orientation.none;
-
-            if ('h' == position.charAt(0) && position.length() > 2)
-                return Orientation.hor;
-
-            else if ('v' == position.charAt(0))
-                return Orientation.ver;
-
-            return Orientation.none;
-        }
-
-        public static String getPosition(Orientation orientation, int x, int y) {
-            return (orientation.toString() + (char) ((int) 'a' + x) + (char) ((int) '9' - y));
-        }
+        return Math.abs(((int) 'a') - ((int) position.charAt(i)));
     }
+
+    public static int getY(String position) {
+        int i = getOrientation(position) == Orientation.none ? 1 : 2;
+
+        if (position == null || position.length() <= i)
+            return 0;
+
+        return ((int) '9') - ((int) position.charAt(i));
+    }
+
+    public static Orientation getOrientation(String position) {
+        if (position == null || position.length() < 1)
+            return Orientation.none;
+
+        if ('h' == position.charAt(0) && position.length() > 2)
+            return Orientation.hor;
+
+        else if ('v' == position.charAt(0))
+            return Orientation.ver;
+
+        return Orientation.none;
+    }
+
+    public static String getPosition(Orientation orientation, int x, int y) {
+        return (orientation.toString() + (char) ((int) 'a' + x) + (char) ((int) '9' - y));
+    }
+}
