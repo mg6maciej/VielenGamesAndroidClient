7e2821b17b0a23c628fa56e689d1cdfe466035a5
Maciej Górski <maciek.gorski@gmail.com>
2014-06-21 21:47:55 +0200

    complementary tests for blocking wall moves

--- app/src/androidTest/java/com/elpassion/vielengames/kuridor/WallMoveValidationBlockingPawnTests.java
+++ app/src/androidTest/java/com/elpassion/vielengames/kuridor/WallMoveValidationBlockingPawnTests.java
@@ -42,6 +42,46 @@ public final class WallMoveValidationBlockingPawnTests extends WallMoveValidatio
         assertNotValid("h7h");
     }
 
+    public void testCanSeparatePlayersOnGoodSides() {
+        testedState = withPlayersAlmostSeparatedOnGoodSides;
+        assertValid("h6h");
+        assertValid("h4h");
+    }
+
+    public void testCannotSeparatePlayersOnWrongSides() {
+        testedState = withPlayersAlmostSeparatedOnWrongSides;
+        assertNotValid("h6h");
+        assertNotValid("h4h");
+    }
+
+    public void testCannotBlockPlayersOnNorthSide() {
+        testedState = withPlayersAlmostSeparatedOnNorthSide;
+        assertNotValid("h6h");
+        assertNotValid("h4h");
+    }
+
+    public void testCannotBlockPlayersOnSouthSide() {
+        testedState = withPlayersAlmostSeparatedOnSouthSide;
+        assertNotValid("h6h");
+        assertNotValid("h4h");
+    }
+
+    public void testCannotCompletelyBoxPawnToNorthOrSouthSide() {
+        testedState = withAlmostBlockedPawnsOnNorthAndSouthSides;
+        assertNotValid("d2h");
+        assertNotValid("e2h");
+        assertNotValid("d7h");
+        assertNotValid("e7h");
+    }
+
+    public void testCannotCompletelyBoxPawnToEastOrWestSide() {
+        testedState = withAlmostBlockedPawnsOnEastAndWestSides;
+        assertNotValid("b4v");
+        assertNotValid("b5v");
+        assertNotValid("g4v");
+        assertNotValid("g5v");
+    }
+
     private KuridorGameState withAlmostBlockedPawn = KuridorGameState.builder()
             .team1(KuridorGameTeamState.builder().pawnPosition("e5").wallsLeft(10).build())
             .team2(KuridorGameTeamState.builder().pawnPosition("e9").wallsLeft(10).build())
@@ -60,6 +100,48 @@ public final class WallMoveValidationBlockingPawnTests extends WallMoveValidatio
                     "c1h", "e1h", "g1h",
                     "h2v", "h4v", "h6v", "h8v",
                     "g8h", "e8h", "c8h", "a8h"))
+            .activeTeam("team_1")
+            .build();
+
+    private KuridorGameState withPlayersAlmostSeparatedOnGoodSides = KuridorGameState.builder()
+            .team1(KuridorGameTeamState.builder().pawnPosition("e6").wallsLeft(5).build())
+            .team2(KuridorGameTeamState.builder().pawnPosition("e5").wallsLeft(5).build())
+            .walls(Arrays.asList("a5h", "c5h", "e5h", "g5h", "h5v"))
+            .activeTeam("team_1")
+            .build();
+
+    private KuridorGameState withPlayersAlmostSeparatedOnWrongSides = KuridorGameState.builder()
+            .team1(KuridorGameTeamState.builder().pawnPosition("e5").wallsLeft(5).build())
+            .team2(KuridorGameTeamState.builder().pawnPosition("e6").wallsLeft(5).build())
+            .walls(Arrays.asList("a5h", "c5h", "e5h", "g5h", "h5v"))
+            .activeTeam("team_1")
+            .build();
+
+    private KuridorGameState withPlayersAlmostSeparatedOnNorthSide = KuridorGameState.builder()
+            .team1(KuridorGameTeamState.builder().pawnPosition("f6").wallsLeft(5).build())
+            .team2(KuridorGameTeamState.builder().pawnPosition("e6").wallsLeft(5).build())
+            .walls(Arrays.asList("a5h", "c5h", "e5h", "g5h", "h5v"))
+            .activeTeam("team_1")
+            .build();
+
+    private KuridorGameState withPlayersAlmostSeparatedOnSouthSide = KuridorGameState.builder()
+            .team1(KuridorGameTeamState.builder().pawnPosition("e5").wallsLeft(5).build())
+            .team2(KuridorGameTeamState.builder().pawnPosition("f5").wallsLeft(5).build())
+            .walls(Arrays.asList("a5h", "c5h", "e5h", "g5h", "h5v"))
+            .activeTeam("team_1")
+            .build();
+
+    private KuridorGameState withAlmostBlockedPawnsOnNorthAndSouthSides = KuridorGameState.builder()
+            .team1(KuridorGameTeamState.builder().pawnPosition("e1").wallsLeft(8).build())
+            .team2(KuridorGameTeamState.builder().pawnPosition("e9").wallsLeft(8).build())
+            .walls(Arrays.asList("d1v", "e1v", "d8v", "e8v"))
+            .activeTeam("team_1")
+            .build();
+
+    private KuridorGameState withAlmostBlockedPawnsOnEastAndWestSides = KuridorGameState.builder()
+            .team1(KuridorGameTeamState.builder().pawnPosition("a5").wallsLeft(8).build())
+            .team2(KuridorGameTeamState.builder().pawnPosition("i5").wallsLeft(8).build())
+            .walls(Arrays.asList("a4h", "a5h", "h4h", "h5h"))
             .activeTeam("team_1")
             .build();
 }
