c78000d7e8e8d4e9fdec4edd53b3f3926fe24e04
Maciej Górski <maciek.gorski@gmail.com>
2014-07-01 20:06:05 +0200

    pawn moves

--- app/src/main/java/com/elpassion/vielengames/ui/kuridor/GameView.java
+++ app/src/main/java/com/elpassion/vielengames/ui/kuridor/GameView.java
@@ -4,7 +4,11 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 import com.elpassion.vielengames.R;
 import com.elpassion.vielengames.data.kuridor.KuridorGameState;
@@ -29,6 +33,8 @@ public final class GameView extends BaseView {
 
     private KuridorGameStateDrawer.Settings drawerSettings;
 
+    private GestureDetector gestureDetector;
+
     @SuppressWarnings("unused")
     public GameView(Context context) {
         super(context);
@@ -51,9 +57,10 @@ public final class GameView extends BaseView {
         Resources r = getResources();
         Paint paint = new Paint();
         paint.setAntiAlias(true);
+        final float gameViewPadding = r.getDimension(R.dimen.game_view_padding);
         drawerSettings = new KuridorGameStateDrawer.Settings()
                 .paint(paint)
-                .padding(r.getDimension(R.dimen.game_view_padding))
+                .padding(gameViewPadding)
                 .dotsRadius(r.getDimension(R.dimen.game_view_dots_radius))
                 .lastLineDotsRadius(r.getDimension(R.dimen.game_view_last_line_dots_radius))
                 .wallWidth(r.getDimension(R.dimen.game_view_wall_width))
@@ -61,6 +68,37 @@ public final class GameView extends BaseView {
                 .pawnPadding(r.getDimension(R.dimen.game_view_pawn_padding))
                 .team1Color(r.getColor(R.color.green_normal))
                 .team2Color(r.getColor(R.color.blue_normal));
+
+        gestureDetector = new GestureDetector(getContext(), new GestureDetector.SimpleOnGestureListener() {
+
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return true;
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                int smaller = Math.min(getWidth(), getHeight());
+                float xPadding = gameViewPadding + (getWidth() - smaller) / 2.0f;
+                float yPadding = gameViewPadding + (getHeight() - smaller) / 2.0f;
+                float x = e.getX();
+                float y = e.getY();
+                if (x < xPadding || y < yPadding
+                        || x >= xPadding + smaller - 1 - 2 * gameViewPadding
+                        || y >= yPadding + smaller - 1 - 2 * gameViewPadding) {
+                    return true;
+                }
+                x -= xPadding;
+                y -= yPadding;
+                Point point = new Point(
+                        (int) (x / ((smaller - 1 - 2 * gameViewPadding) / 9.0f)),
+                        (int) (y / ((smaller - 1 - 2 * gameViewPadding) / 9.0f))
+                );
+                Log.e("move", "" + (char) ('a' + point.x) + (char) ('9' - point.y));
+                moveListener.onMove(KuridorMove.pawn("" + (char) ('a' + point.x) + (char) ('9' - point.y)));
+                return true;
+            }
+        });
     }
 
     public void setState(KuridorGameState state) {
@@ -75,5 +113,10 @@ public final class GameView extends BaseView {
                 .width(getWidth())
                 .height(getHeight());
         KuridorGameStateDrawer.draw(state, canvas, drawerSettings);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
     }
 }
