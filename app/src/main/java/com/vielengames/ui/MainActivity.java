dfb74bf02e19985b485c5efaf2b91e77c458c855
Maciej Górski <maciek.gorski@gmail.com>
2014-07-13 17:59:01 +0200

    sign out action

--- app/src/main/java/com/vielengames/ui/MainActivity.java
+++ app/src/main/java/com/vielengames/ui/MainActivity.java
@@ -2,8 +2,11 @@ package com.vielengames.ui;
 
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.facebook.Session;
 import com.vielengames.ForegroundNotifier;
 import com.elpassion.vielengames.R;
 import com.vielengames.VielenGamesPrefs;
@@ -62,6 +65,31 @@ public final class MainActivity extends BaseActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.main_sign_out:
+                signOut();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void signOut() {
+        if (Session.getActiveSession() != null) {
+            Session.getActiveSession().close();
+        }
+        finish();
     }
 
     @Override
