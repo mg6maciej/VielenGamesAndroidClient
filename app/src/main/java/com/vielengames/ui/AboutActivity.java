03f9e08c47b1c4346d82ff44ce5e864e34e2366c
Maciej Górski <maciek.gorski@gmail.com>
2014-07-13 20:27:01 +0200

    about screen filled with people

--- app/src/main/java/com/vielengames/ui/AboutActivity.java
+++ app/src/main/java/com/vielengames/ui/AboutActivity.java
@@ -1,4 +1,31 @@
 package com.vielengames.ui;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.vielengames.R;
+import com.vielengames.utils.ViewUtils;
+
 public final class AboutActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_activity);
+        final AboutListAdapter adapter = new AboutListAdapter(this);
+        ListView listView = ViewUtils.findView(this, R.id.about_list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                String url = (String) adapter.getItem(position);
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+    }
 }
---
+++ app/src/main/java/com/vielengames/ui/AboutListAdapter.java
@@ -0,0 +1,120 @@
+package com.vielengames.ui;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.DrawableRes;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.vielengames.R;
+import com.vielengames.ui.common.AbstractBaseAdapter;
+import com.vielengames.ui.common.ItemAdapter;
+import com.vielengames.utils.Circlifier;
+import com.vielengames.utils.ViewUtils;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+public final class AboutListAdapter extends AbstractBaseAdapter {
+
+    public AboutListAdapter(Context context) {
+        super(context);
+        items.add(new AboutHeaderItemAdapter("DESIGNER"));
+        items.add(new AboutPersonItemAdapter("Natalia Berowska", R.drawable.about_image_natalia_b, "https://www.behance.net/natiwa"));
+        items.add(new AboutHeaderItemAdapter("ANDROID DEVELOPER"));
+        items.add(new AboutPersonItemAdapter("Maciej Górski", R.drawable.about_image_maciej_g, "https://github.com/mg6maciej"));
+        items.add(new AboutHeaderItemAdapter("BACKEND DEVELOPER"));
+        items.add(new AboutPersonItemAdapter("Michał Janeczek", R.drawable.about_image_michal_j, "http://mjaneczek.pl"));
+    }
+
+    @RequiredArgsConstructor
+    private static final class AboutHeaderItemAdapter implements ItemAdapter {
+
+        private final String title;
+
+        @Override
+        public Object getItem() {
+            return null;
+        }
+
+        @Override
+        public long getItemId() {
+            return 0;
+        }
+
+        @Override
+        public int getLayoutId() {
+            return R.layout.about_header_item;
+        }
+
+        @Override
+        public void bindView(View itemView) {
+            TextView titleView = (TextView) itemView;
+            titleView.setText(title);
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return false;
+        }
+
+        @Override
+        public int getItemViewType() {
+            return 0;
+        }
+    }
+
+    @RequiredArgsConstructor
+    private static final class AboutPersonItemAdapter implements ItemAdapter {
+
+        private final String name;
+        private final int image;
+        private final String url;
+        private Bitmap circlified;
+
+        @Override
+        public String getItem() {
+            return url;
+        }
+
+        @Override
+        public long getItemId() {
+            return 0;
+        }
+
+        @Override
+        public int getLayoutId() {
+            return R.layout.about_person_item;
+        }
+
+        @Override
+        public void bindView(View itemView) {
+            if (circlified == null) {
+                circlified = createCirclifiedBitmap(itemView.getResources());
+            }
+            ViewUtils.setImage(circlified, itemView, R.id.about_person_image);
+            ViewUtils.setText(name, itemView, R.id.about_person_name);
+            ViewUtils.setText(url, itemView, R.id.about_person_url);
+        }
+
+        private Bitmap createCirclifiedBitmap(Resources resources) {
+            final Bitmap bitmap = BitmapFactory.decodeResource(resources, image);
+            final int circleColor = 0xFFEEEEEE;
+            final float circleWidth = resources.getDimension(R.dimen.common_circle_width);
+            return Circlifier.circlify(bitmap, circleColor, circleWidth);
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return url != null;
+        }
+
+        @Override
+        public int getItemViewType() {
+            return 1;
+        }
+    }
+}
