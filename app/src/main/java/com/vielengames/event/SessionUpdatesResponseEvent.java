c461d8178472829f14083e959b0fbafd254cca59
Maciej Górski <maciek.gorski@gmail.com>
2014-07-17 16:15:07 +0200

    removed unused code

--- app/src/main/java/com/vielengames/event/SessionUpdatesResponseEvent.java
+++ app/src/main/java/com/vielengames/event/SessionUpdatesResponseEvent.java
@@ -4,10 +4,8 @@ import com.vielengames.data.Updates;
 
 import lombok.Value;
 
-/**
- * Created by pawel on 16.06.14.
- */
 @Value
 public class SessionUpdatesResponseEvent {
+
     Updates updates;
 }
