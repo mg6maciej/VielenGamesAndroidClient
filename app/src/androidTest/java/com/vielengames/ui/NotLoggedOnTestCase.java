240fc316a9845ca9ab35de48ecbc5f6cc06e16a6
Maciej Górski <maciek.gorski@gmail.com>
2015-01-21 04:32:10 +0100

    Failing login test.

--- app/src/androidTest/java/com/vielengames/ui/NotLoggedOnTestCase.java
+++ app/src/androidTest/java/com/vielengames/ui/NotLoggedOnTestCase.java
@@ -3,13 +3,20 @@ package com.vielengames.ui;
 import com.vielengames.R;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public final class NotLoggedOnTestCase extends BaseInstrumentationTestCase {
 
     public void testNotLoggedOnInitialState() {
         onView(withId(R.id.login_buttons_container)).check(matches(isDisplayed()));
+    }
+
+    public void testLoggedOnAfterLoginClick() {
+        onView(withText("Login with Stub")).perform(click());
+        onView(withId(R.id.main_add_proposal_button)).check(matches(isDisplayed()));
     }
 }
